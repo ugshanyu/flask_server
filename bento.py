198af9d2a635e968817f023271ec09e6c4ba02af
Uuganbayar <88976978+ugshanyu@users.noreply.github.com>
2024-02-27 19:22:44 +0800

    Add files via upload

---
+++ bento.py
@@ -0,0 +1,38 @@
+import bentoml
+
+from typing import Optional, AsyncGenerator, List
+
+MAX_TOKENS = 512
+PROMPT_TEMPLATE = """{user_prompt}"""
+
+@bentoml.service(
+    traffic={
+        "timeout": 300,
+    },
+    resources={
+        "gpu": 1,
+        "memory": "16Gi",
+    },
+)
+class VLLM:
+    def __init__(self) -> None:
+        from vllm import AsyncEngineArgs, AsyncLLMEngine
+
+        ENGINE_ARGS = AsyncEngineArgs(
+            model='meta-llama/Llama-2-7b-chat-hf',
+            max_model_len=MAX_TOKENS
+        )
+
+        self.engine = AsyncLLMEngine.from_engine_args(ENGINE_ARGS)
+        self.request_id = 0
+
+    @bentoml.api
+    async def generate(self, prompt: str = "Explain superconductors like I'm five years old", tokens: Optional[List[int]] = None) -> AsyncGenerator[str, None]:
+        from vllm import SamplingParams
+
+        SAMPLING_PARAM = SamplingParams(max_tokens=MAX_TOKENS)
+        prompt = PROMPT_TEMPLATE.format(user_prompt=prompt)
+        stream = await self.engine.add_request(self.request_id, prompt, SAMPLING_PARAM, prompt_token_ids=tokens)
+        self.request_id += 1
+        async for request_output in stream:
+            yield request_output.outputs[0].text
