d692eb8bd22aa9d6c26ba4d601552a53ce4aa2a9
Uuganbayar <88976978+ugshanyu@users.noreply.github.com>
2024-02-29 11:00:37 +0800

    Update my-server.py

--- my-server.py
+++ my-server.py
@@ -23,5 +23,14 @@ async def my_event(sid, message):
         await sio.emit('my_response', {'data': generation.outputs[0].text}, room=sid)
     sio.emit('my_response', {'data': "<end>"}, room=sid)
 
+@sio.event
+async def all(sid, message):
+    print("User said: " + message['data'])
+    generated_parts = []
+    async for generation in llm.generate_iterator(message['data']):
+        generated_parts.append(generation.outputs[0].text)
+    full_generated_text = ''.join(generated_parts)
+    await sio.emit('my_response', {'data': full_generated_text}, room=sid)
+
 if __name__ == '__main__':
     web.run_app(app, port=8080)
