ed89849e8c73dfa6b9ccfcd780aafb408fc78009
Uuganbayar <88976978+ugshanyu@users.noreply.github.com>
2024-02-29 08:29:23 +0800

    Update my-server.py

--- my-server.py
+++ my-server.py
@@ -1,9 +1,12 @@
 from aiohttp import web
 import socketio
+import openai_llm as openllm
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
+
+llm = openllm.LLM('ugshanyu/mongol-mistral-3')
 
 @sio.event
 async def connect(sid, environ):
@@ -15,8 +18,9 @@ async def disconnect(sid):
 
 @sio.event
 async def my_event(sid, message):
-    print('User said' + message['data'], sid)
-    await sio.emit('my_response', {'data': 'Hello!'}, room=sid)
+    print("User said" + message['data'])
+    async for generation in llm.generate_iterator(message['data']):
+        await sio.emit('my_response', {'data': generation.outputs[0].text}, room=sid)
 
 if __name__ == '__main__':
     web.run_app(app, port=8080)
